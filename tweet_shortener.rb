2245ae0aaf9ab0763a086523a0e2e56d726228f3
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-13 08:16:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+
+def dictionary
+dictionary= {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+} 
+end 
+
+def word_substituter(tweet)
+  tweet_a= tweet.split.collect do |words|
+if dictionary.keys.include?(words.downcase)
+      words = dictionary[words.downcase]
+    else words 
+  end
+end 
+tweet_a.join(" ")
+end 
+
+def bulk_tweet_shortener
+end 
+
+def selective_tweet_shortener 
+end 
+
+shortened_tweet_truncator 
+end 
+
+  
